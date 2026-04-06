d3f4cefd33d3f8d51e87b90576f6692b614b5b5f
Dawn Pham <danh.pham@polaris-edge.com>
2026-04-06 10:19:28 +0700

    feat(session-1): scaffold React learning app with Vite tooling

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { AppProviders } from './providers/AppProviders';
+import { AppRouter } from './router/AppRouter';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppProviders>
+      <AppRouter />
+    </AppProviders>
+  </React.StrictMode>,
+);
